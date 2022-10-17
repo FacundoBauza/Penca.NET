8b124601d7e68324142869ce31d360f008e3f1d6
Facundo Bauza <fbauza2014@gmail.com>
2022-10-17 07:47:28 -0300

    PrimerCommit

---
+++ Clases/Entidades/Torneo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases.Entidades
+{
+    public class Torneo
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public DateTime fechaInicio { get; set; }
+        public DateTime fechaFin { get; set; }
+
+    }
+}
---
+++ Clases/Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases.Entidades
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public string nickname { get; set; }
+        public string password { get; set; }
+        public Subscripcion subscripcion { get; set; }
+
+
+    }
+}
