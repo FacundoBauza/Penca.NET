73de1a437698068e3dca3f3fc431798091d444a4
fcamilo23 <facundo.camilo@estudiantes.utec.edu.uy>
2022-10-19 20:00:07 -0300

    Update Evento.cs

--- Clases/Entidades/Evento.cs
+++ Clases/Entidades/Evento.cs
@@ -8,5 +8,13 @@ namespace Dominio.Entidades
 {
     public class Evento
     {
+		public int id { get; set; }
+        public string equipo1 { get; set; }
+		public string equipo2 { get; set; }
+        public DateTime fechaHora{ get; set; }
+		public string golesEquipo1 { get; set; }
+		public string golesEquipo2 { get; set; }
+		public string resultado { get; set; }
+
     }
 }
